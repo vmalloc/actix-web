5b138b520424cac2fd1e4405c1a6f7aef5c3258e
Travis CI User <travis@example.org>
2019-03-24 19:15:34 +0000

    Documentation upload

---
+++ implementors/actix_router/trait.Resource.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = [{text:"impl&lt;P&gt; Resource&lt;<a class=\"struct\" href=\"actix_web/dev/struct.Url.html\" title=\"struct actix_web::dev::Url\">Url</a>&gt; for <a class=\"struct\" href=\"actix_web/dev/struct.ServiceRequest.html\" title=\"struct actix_web::dev::ServiceRequest\">ServiceRequest</a>&lt;P&gt;",synthetic:false,types:["actix_web::service::ServiceRequest"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
